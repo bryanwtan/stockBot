5b2689e4fd8d1a75786e6d2c4dfa4237de5c9fc3
BryanTan <bryantan@Bryans-MacBook-Pro.local>
2017-09-15 18:28:42 -0400

    cleanup

---
+++ finviz.js
@@ -0,0 +1,13 @@
+module.exports.finvizQuery = [
+	[
+		'v=111', // version
+		'f=cap_smallover' // market cap over 300
+	].join('&'),
+	'geo_usa', // usa companies
+	'sh_curvol_o200', // current volume over 200k
+	[
+		'sh_insidertrans_verypos', // very positive insider trades (+20%)
+		'ft=4', // optional filter set 4 (all available filters)
+		'o=-marketcap' // sort by largest to smallest market capitalization
+	].join('&')
+].join(',')
--- server.js
+++ server.js
@@ -1,44 +1,11 @@
 const _ = require('lodash')
 const fs = require('fs')
-const { stringify, applyType, request, getInfo } = require('./utils.js')
+const { stringify, applyType, getInfo } = require('./utils.js')
+const { finvizQuery } = require('./finviz.js')
+const { getSortedStocks, getDetails } = require('./dataRetrieval.js')
 
-const getSortedStocks = async query => {
-	const url = `http://finviz.com/screener.ashx?${query}`
-	const parent = '.table-dark-row-cp, .table-light-row-cp'
-	const children = [['td']]
-
-	const header = await request({ url, selectors: { parent: ['.table-top'] } })
-	const data = await request({ url, selectors: { parent, children } })
-	return data.map(stock => _.zipObject(header, stock))
-}
-
-const getDetails = async stock => {
-	const url = `http://finviz.com/quote.ashx?t=${stock.Ticker}`
-	const parent = '.snapshot-table2'
-	const children = ['td']
-
-	const details = await request({ url, selectors: { parent, children } })
-
-	return Object.assign({}, stock, _.fromPairs(_.chunk(details, 2)))
-}
-
-const getEnrichedStockData = async () => {
-	const query = [
-		[
-			'v=111', // version
-			'f=cap_smallover' // market cap over 300
-		].join('&'),
-		'geo_usa', // usa companies
-		'sh_curvol_o200', // current volume over 200k
-		[
-			'sh_insidertrans_verypos', // very positive insider trades (+20%)
-			'ft=4', // optional filter set 4 (all available filters)
-			'o=-marketcap' // sort by largest to smallest market capitalization
-		].join('&')
-	].join(',')
-
+const getEnrichedStockData = async query => {
 	const stocks = await getSortedStocks(query)
-
 	const originalData = await Promise.all(stocks.map(stock => getDetails(stock)))
 
 	const bestStocks = await Promise.all(
@@ -54,4 +21,4 @@ const getEnrichedStockData = async () => {
 	console.info(`data for ${bestStocks.length} saved`)
 }
 
-getEnrichedStockData()
+getEnrichedStockData(finvizQuery)
